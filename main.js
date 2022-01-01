29c0a5f5f2e69f8c45f6f93d4adea33298599568
sadamczyk <13919759+sadamczyk@users.noreply.github.com>
2022-01-01 18:54:12 +0100

    Remove unused code

--- main.js
+++ main.js
@@ -4,27 +4,12 @@ let roleBuilder = require('role.builder')
 let roleSpawn = require('role.spawn')
 let roleTower = require('role.tower')
 let roleWorker = require('role.worker')
-let util = require('util')
 
 // TODOs:
 // Add construction sites and build buildings
 
 
 module.exports.loop = function () {
-    // This should only ever run once per "active" room
-    for (const name in Game.rooms) {
-        const room = Game.rooms[name]
-        if (!room.memory.sources) {
-            const sources = room.find(FIND_SOURCES)
-            let results = []
-            for (const source of sources) {
-                results.push({ id: source.id, freeTiles: util.getFreeTiles(source.pos) })
-            }
-            room.memory.sources = results
-            console.log('Wrote ' + sources.length + ' sources into memory')
-        }
-    }
-    
     // Cleanup memory
     for (let name in Memory.creeps) {
         if (!Game.creeps[name]) {
